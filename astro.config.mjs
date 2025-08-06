502c0e517d563ebf66aa448a2764399b0e69a1c2
Nathan Snook <54990015+w0418773@users.noreply.github.com>
2025-08-06 13:21:41 -0300

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://w0418773.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
